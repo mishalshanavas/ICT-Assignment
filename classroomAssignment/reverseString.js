80d381e5d7637fa5926580c809efab37245d89c8
mishalshanavas <mishalshanavas8@gmail.com>
2025-08-17 19:34:02 +0530

    Add: classroom assignment5

---
+++ classroomAssignment/reverseString.js
@@ -0,0 +1,14 @@
+/*
+function reverseString(str) {
+    return str.reverse();
+}
+*/
+function reverseString(str) {
+    let reversed = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed = reversed + str[i];
+    }
+    return reversed;
+}
+
+console.log(reverseString("hello"));
