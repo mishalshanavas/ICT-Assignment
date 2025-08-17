3dc077e68eab466e98c9b07cb323b919e95717c8
mishalshanavas <mishalshanavas8@gmail.com>
2025-08-17 19:27:26 +0530

    Add: classroom assignment4

---
+++ classroomAssignment/oddEven.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+console.log(checkEvenOdd(4));
+console.log(checkEvenOdd(7));
