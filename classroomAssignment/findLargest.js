08e6d74a00aa94c16b0a4c3990f1acc564adf2d8
mishalshanavas <mishalshanavas8@gmail.com>
2025-08-17 19:24:02 +0530

    Add: classroom assignment3

---
+++ classroomAssignment/findLargest.js
@@ -0,0 +1,11 @@
+function findLargest(numbers) {
+    let largest = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > largest) {
+            largest = numbers[i];
+        }
+    }
+    return largest;
+}
+
+console.log(findLargest([3, 7, 2, 9, 1]));
